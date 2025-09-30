42de0fbdf9331ccc1c9af9fe658a53192130742a
dinesh9077 <94532938+dinesh9077@users.noreply.github.com>
2025-09-30 15:13:21 +0530

    te

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,7 @@
 	
 	use Illuminate\Http\Request;
 	use App\Models\Customer;
-	use App\Models\order;
+	use App\Models\Order;
 	use App\Models\CustomerAddress;
 	use App\Models\CourierWarehouse; 
 	use Illuminate\Support\Facades\DB;
